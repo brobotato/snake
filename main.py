230e7db01d6d6f1732f1eb0e9b9897376968c285
BrianUser <brianxu01@gmail.com>
2017-02-13 19:44:11 -0800

    it's pretty much done i don't know what else to say added death added (apples? i dunno) added score

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, sys, pygame, math
+import os, sys, pygame, math, random
 from pygame.locals import *
 
 pygame.init()
@@ -37,12 +37,25 @@ def display_data(x, y, data, font, color):
     window_surface_obj.blit(datatext, (x, y))
 
 
-snake = [[20, 15], [21, 15], [22, 15], [23, 15]]
+def reseed():
+    x = random.randint(0, 39)
+    y = random.randint(0, 29)
+    for s in snake:
+        if [x, y] != s:
+            return [x, y]
+        else:
+            return reseed()
+
+
+snake = [[19, 15], [20, 15], [20, 15], [20, 15]]
+apple = reseed()
 direction = 180
+score = 0
 locations = {}
 for x in range(1, len(snake), 1):
     locations[x] = snake[x - 1]
 time = 0
+turned = 0
 # auto fill dictionary with sprites from resources
 img_dict = {}
 for filename in os.listdir('resources'):
@@ -56,29 +69,48 @@ while not crashed:
             pygame.quit()
             sys.exit()
     if pygame.key.get_pressed()[pygame.K_UP] != 0:
-        if direction != 90:
+        if direction != 90 and turned == 0:
             direction = -90
+            turned = 1
     if pygame.key.get_pressed()[pygame.K_LEFT] != 0:
-        if direction != 0:
+        if direction != 0 and turned == 0:
             direction = 180
+            turned = 1
     if pygame.key.get_pressed()[pygame.K_RIGHT] != 0:
-        if direction != 180:
+        if direction != 180 and turned == 0:
             direction = 0
+            turned = 1
     if pygame.key.get_pressed()[pygame.K_DOWN] != 0:
-        if direction != -90:
+        if direction != -90 and turned == 0:
             direction = 90
-    if time % 15 == 0:
+            turned = 1
+    if time % (15 - score) == 0:
         for x in range(0, len(snake), 1):
-            locations[x] = [snake[x][0],snake[x][1]]
+            locations[x] = [snake[x][0], snake[x][1]]
             if x != 0:
                 snake[x] = locations[x - 1]
             else:
                 snake[0][0] += math.cos(math.radians(direction))
                 snake[0][1] += math.sin(math.radians(direction))
+                turned = 0
+    render(apple[0], apple[1], img_dict['block'])
+    display_data(0, 0, score, font_obj, white)
+    for x in range(1, len(snake), 1):
+        if snake[0] == snake[x]:
+            crashed = True
+    if 0 < snake[0][0] < 40 and 0 < snake[0][1] < 30:
+        pass
+    else:
+        crashed = True
     for s in snake:
         render(s[0], s[1], img_dict['block'])
-    display_data(0, 0, direction, font_obj, white)
-    display_data(0, 10, locations, font_obj, white)
+        s[0] = round(s[0])
+        s[1] = round(s[1])
+    if snake[0] == apple:
+        snake.append([snake[len(snake) - 1][0], snake[len(snake) - 1][1]])
+        apple = reseed()
+        if score < 15:
+            score += 1
     pygame.display.update()
     time += 1
     fps_clock.tick(30)
